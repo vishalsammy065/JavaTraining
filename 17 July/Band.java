812a4e8916e5ea9aa5a05608f86057e33b857829
Vishal <vishal.sammy065@gmail.com>
2018-07-17 12:41:43 +0530

    Create Band.java

---
+++ 17 July/Band.java
@@ -0,0 +1,45 @@
+
+public class Band {
+	private String bandName;
+	private String[] singers;
+	private String[] guitarists;
+	private String[] drummer;
+	private String[] keyboardPlayer;
+	
+	public String getBandName() {
+		return bandName;
+	}
+	
+	public void setBandName(String bandName, String[] singers, String[] guitarists, String[] drummer, String[] keyboardPlayer) {
+		this.bandName = bandName;
+		this.singers = singers;
+		this.guitarists = guitarists;
+		this.drummer = drummer;
+		this.keyboardPlayer = keyboardPlayer;
+	}
+	
+	public void printBandDetails() {
+		System.out.println();
+		System.out.println("Band Name: "+this.bandName);
+		System.out.println("Singers are : ");
+		for(String i : this.singers) {
+			System.out.println(i);
+		}
+		System.out.println();
+		System.out.println("Guitarists are : ");
+		for(String i : this.guitarists) {
+			System.out.println(i);
+		}
+		System.out.println();
+		System.out.println("Drummers are : ");
+		for(String i : this.drummer) {
+			System.out.println(i);
+		}
+		System.out.println();
+		System.out.println("Keyboard Players are : ");
+		for(String i : this.keyboardPlayer) {
+			System.out.println(i);
+		}
+	}
+	
+}

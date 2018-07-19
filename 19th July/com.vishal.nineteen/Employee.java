25b0e4fb9084886e817c7a00f2ccdc77ab203e76
Vishal <vishal.sammy065@gmail.com>
2018-07-19 16:37:49 +0530

    Update Employee.java

--- 19th July/com.vishal.nineteen/Employee.java
+++ 19th July/com.vishal.nineteen/Employee.java
@@ -4,7 +4,31 @@ package com.vishal.nineteen;
  * 
  */
 public class Employee {
+	int empId;
+	double salary;
+	String dept;
+	Address add;
+	public Employee(int empId, double salary, String dept, Address add) {
+		super();
+		this.empId = empId;
+		this.salary = salary;
+		this.dept = dept;
+		this.add = add;
+	}
 	
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empId= " + empId + ", salary= " + salary + ", dept= " + dept + ", add= " + add + "]";
+	}
+	
+	public static void main(String[] arsg) {
+		Employee e = new Employee(101, 50000, "IT", new Address("101", "Main Street", "Banglore"));
+		System.out.println(e);
+	}
 }
 
 

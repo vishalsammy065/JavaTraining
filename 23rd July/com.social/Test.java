47df36bff539ca950a46b24fc038191357cecbd5
Vishal <vishal.sammy065@gmail.com>
2018-07-23 12:34:59 +0530

    Update Test.java

--- 23rd July/com.social/Test.java
+++ 23rd July/com.social/Test.java
@@ -5,11 +5,18 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		TwitterFeedReader tf = new TwitterFeedReader();
-		Message m = tf.pollForNewMessage();
-		System.out.println(m.getMessageId());
-		System.out.println(m.getMessageType());
-		System.out.println(m.getMessageSource());
-		System.out.println(m.getMessage());
+		Message twitterMessageObject = tf.pollForNewMessage();
+		System.out.println(twitterMessageObject.getMessageId());
+		System.out.println(twitterMessageObject.getMessageType());
+		System.out.println(twitterMessageObject.getMessageSource());
+		System.out.println(twitterMessageObject.getSender());
+		System.out.println(twitterMessageObject.getMessage()+"\n\n");
+		 
+		Message facebookMessageObject = (new FacebookFeedReader()).pollForNewMessage();
+		System.out.println(facebookMessageObject.getMessageId());
+		System.out.println(facebookMessageObject.getMessageType());
+		System.out.println(facebookMessageObject.getMessageSource());
+		System.out.println(facebookMessageObject.getSender());
+		System.out.println(facebookMessageObject.getMessage());
 	}
-
 }
